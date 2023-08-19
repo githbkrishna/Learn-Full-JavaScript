03277de129cddef779d8c01064cc4d544b273628
githbkrishna <139392000+githbkrishna@users.noreply.github.com>
2023-08-20 01:52:05 +0530

    commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// console.log(Hello);   //single line comment
+
+
+/* console.log("Hello");
+    console.log("Hello");
+    console.log("Hello");
+    console.log("Hello");   //multi-line comment
+*/
